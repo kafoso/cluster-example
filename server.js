416e0126c96822e5f7c6683e2ce93bbd42a13766
kafoso <soefritz@gmail.com>
2015-07-03 23:47:00 +0200

    Improve worker communication on startup

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var cluster = require('cluster');
 var sprintf = require('sprintf-js').sprintf;
 
+var getWorkerOutput = function(w){
+  return sprintf("[id: %s] [pid: %s]", w.id, w.process.pid);
+};
+
 if (cluster.isMaster) {
   var cpuCount = require('os').cpus().length;
   console.log("Starting server. CPU count: %s", cpuCount);
@@ -9,9 +13,6 @@ if (cluster.isMaster) {
   }
   cluster.on('exit', function(deadWorker, code, signal) {
     var worker = cluster.fork(); // Restart the worker
-    var getWorkerOutput = function(w){
-      return sprintf("[id: %s] [pid: %s]", w.id, w.process.pid);
-    };
     console.log("Worker died: %s [code: %s] [signal: %s]. New worker started: %s.",
       getWorkerOutput(deadWorker), code, signal, getWorkerOutput(worker));
   });
@@ -21,5 +22,6 @@ if (cluster.isMaster) {
     cluster.workers[workersKeys[randomWorker]].kill();
   }, 1000); // Kill a random worker every so often to verify the server keeps running
 } else {
+  console.log("Worker started: %s", getWorkerOutput(cluster.worker));
   require("./server/bootstrap.js")(cluster.worker);
 }
